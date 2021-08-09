f97610b1c13ebf600b2bcc69f8979c30dedd4de1
VasylPerepichai <vperepichay@gmail.com>
2021-08-09 11:14:13 +0300

    Updates

---
+++ precache-manifest.3b8f987a22cb1dde13f993c7448a88c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b928ed863e2b1c66dcb1f0d7d59948",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee967a3656bfbbd4196a",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee967a3656bfbbd4196a",
+    "url": "./static/js/main.0fb26718.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
